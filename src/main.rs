67888d5ed1e282854838e2c91b064fba05289e54
Shane M Mathews <shane.m.mathews@gmail.com>
2024-05-18 10:45:50 -0400

    CreateRandomShuffle can now write to file or to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env::{args, Args};
+use std::fs::File;
+use std::io;
 use std::path::PathBuf;
 use std::str::FromStr;
 use strum_macros::EnumString;
+use roaring::RoaringBitmap;
+use rand::Rng;
 
 #[derive(EnumString, Debug)]
 enum Action {
@@ -16,11 +20,33 @@ impl Action {
                 let max_bits = u32::from_str(arg_iter.next().expect("no max_bits given").as_str()).unwrap();
                 let file_name = arg_iter.next().expect("no file_name given");
 
-                if file_name == "--" {
-                    println!("creating a roaring bitmap with {set_bits} bits set in {max_bits} and forward")
+
+
+                let mut bitmap = RoaringBitmap::new();
+                // add 0 through set_bits-1
+                bitmap.insert_range(0..set_bits);
+
+                // randomly shuffle those values throughout the max_bits
+                // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+                let mut rng = rand::thread_rng();
+                for i in (0..max_bits).rev() {
+                    let j = rng.gen_range(0..=i);
+                    let jb = bitmap.contains(j);
+                    let ib = bitmap.contains(i);
+                    if jb && !ib {
+                        bitmap.insert(i);
+                        bitmap.remove(j);
+                    } else if ib && !jb {
+                        bitmap.insert(j);
+                        bitmap.remove(i);
+                    }
+                }
+
+                if file_name == "|" {
+                    bitmap.serialize_into(io::stdout()).unwrap();
                 } else {
                     let path = PathBuf::from_str(file_name.as_str()).unwrap();
-                    println!("creating a roaring bitmap with {set_bits} bits set in {max_bits} and save to {:?}", path)
+                    bitmap.serialize_into(File::create(path).unwrap()).unwrap();
                 }
             }
         }
